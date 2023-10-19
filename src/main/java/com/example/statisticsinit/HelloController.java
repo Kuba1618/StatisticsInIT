200b57ca246b867ae5d561b05b19eb8c257c6243
Kuba1618 <jakub.prusicki1.618@gmail.com>
2023-10-19 20:25:07 +0200

    Read txt file works fine !

--- src/main/java/com/example/statisticsinit/HelloController.java
+++ src/main/java/com/example/statisticsinit/HelloController.java
@@ -1,14 +1,41 @@
 package com.example.statisticsinit;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+    @FXML
+    private Button wczytajCSVBtn;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+    @FXML
+    protected void onWczytajCSVBtnClick(){
+        //String path = "src\\example111.csv";
+        String path = "D:\\PROGRAMMING_PROJECTS\\StatisticsInIT\\StatisticsInIT\\src\\main\\resources\\com\\example\\statisticsinit\\aaa.txt";
+
+        String line = "";
+
+        try {
+                Scanner s1 = new Scanner(new File(path)).useDelimiter(System.lineSeparator());
+                while (s1.hasNextLine()){
+                    line = line + " " + s1.nextLine();
+                }
+        }catch (FileNotFoundException ex) {
+            line = null;
+        }
+
+        System.out.println(line);
+
+    }
+
 }
